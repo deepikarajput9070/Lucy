dec030cb9c24c33fd7e9055e74acb3963c96f8c6
deepikarajput9070 <deepikarajput9070@proton.me>
2026-08-14 10:50:33 +0530

    Fix route definitions and update user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -13,16 +13,25 @@ import {
 } from "../controllers/use.controller.js";
 
 import isAuth from "../middleware/isAuth.js";
-
 import upload from "../middleware/multer.js";
 
 const userRouter = express.Router();
-userRouter.get("/current",isAuth,getCurrentUser);
-userRouter.post("/update",isAuth,upload.single("assistantImage"),updateAssistant);
-userRouterr.post("/logout",isAuth,logOut);
-userRouterr.post("/signin",isAuth,signIn);
-userRouterr.post("/signout",isAuth,signOut);
-userRouter.post("/asktoassistant",isAuth,askToAssistant);
 
+userRouter.get("/current", isAuth, getCurrentUser);
+
+userRouter.post(
+  "/update",
+  isAuth,
+  upload.single("assistantImage"),
+  updateAssistant
+);
+
+userRouter.post("/logout", Logout);
+
+userRouter.post("/signin", Login);
+
+userRouter.post("/signup", signUp);
+
+userRouter.post("/asktoassistant", isAuth, askToAssistant);
 
 export default userRouter;
